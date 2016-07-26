14758820ea63288eb9b836f6a0cddfad3dfbd04a
Tim Martin <tim@timmartin.me>
2016-07-26 18:14:16 -0400

    Bump version: 0.1.0 → 0.1.1

--- case_insensitive_dict.py
+++ case_insensitive_dict.py
@@ -6,7 +6,7 @@ import collections
 
 __author__ = 'Tim Martin'
 __email__ = 'tim@timmartin.me'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 class CaseInsensitiveDict(collections.MutableMapping):
