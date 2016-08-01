f17879e55c7a41aeee8228b924e0ece0e55601a2
Tim Martin <tim@timmartin.me>
2016-08-01 10:00:10 -0400

    Bump version: 0.1.4 → 0.1.5

--- insensitive_dict.py
+++ insensitive_dict.py
@@ -6,7 +6,7 @@ import collections
 
 __author__ = 'Tim Martin'
 __email__ = 'tim@timmartin.me'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 
 class CaseInsensitiveDict(collections.MutableMapping):
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='insensitive-dict',
-    version='0.1.4',
+    version='0.1.5',
     description="A case insensitive dictionary that's fully tested.  Primarily broken out from the requests package",
     long_description=readme + '\n\n' + history,
     author="Tim Martin",
